6c503d3c0da0ad0a0d65909b0260f5874280f700
Kyohei <tatsukawa929@gmail.com>
2020-12-25 23:25:13 +0900

    set jupyter as interactive python

--- init.el
+++ init.el
@@ -32,6 +32,11 @@
 
 (elpy-enable)
 (setq elpy-rpc-virtualenv-path 'current)
+(setq python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+	     "jupyter")
 
 (require 'company)
 (global-company-mode)
