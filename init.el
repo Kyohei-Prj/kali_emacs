ba1e219f2e76d85a4c28ea9194c09a4e40865545
Kyohei <tatsukawa929@gmail.com>
2021-01-24 11:12:15 +0900

    theme

--- init.el
+++ init.el
@@ -22,6 +22,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default))
  '(ein:output-area-inlined-images t)
  '(ivy-rich-mode t)
  '(package-selected-packages
